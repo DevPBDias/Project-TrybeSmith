4202513bcddb23c4cdb916a52e1e2dfb19e3b86a
DevPBDias <devpbdias@gmail.com>
2022-09-14 11:23:03 -0300

    feat(req 7): criando validaçoes para endpoint de cadastro de usuarios

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
 import userControllers from '../controllers/userController';
+import validateUser from '../middlewares/validateUser';
 
 const router = Router();
 
-router.post('/', userControllers.create);
+router.post(
+  '/', 
+  validateUser.validateClasse, 
+  validateUser.validateLevel,
+  validateUser.validatePassword, 
+  validateUser.validateUsername,
+  userControllers.create,
+);
 
 export default router;
